45858ed1db8ca6c6a558c65629adc9a323aedc7e
Marcelo Rojas <maa.rojasd@gmail.com>
2020-08-01 18:51:36 -0400

    initial project

---
+++ src/main/java/com/db/configuration/MsDbJavaTestApplication.java
@@ -0,0 +1,13 @@
+package com.db.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDbJavaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsDbJavaTestApplication.class, args);
+	}
+
+}
